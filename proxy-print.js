5332bf189844acde8500bc7afb1fb039f2201a76
goblincookies <goblincookiebox@gmail.com>
2025-02-20 18:20:34 -0500

    full functionality; basic visuals; basic edge case testing

--- proxy-print.js
+++ proxy-print.js
@@ -1,7 +1,12 @@
-console.log("hello World!")
 
-document.getElementById("card-upload").addEventListener('change', upload);
 
+const card = "card"
+let cardID = 0;
+let cardCount = 0;
+
+function setup() {
+    listenersCreate();
+};
 
 
 function upload(e) {
@@ -11,11 +16,180 @@ function upload(e) {
         console.log("hello!");
         let fr = new FileReader();
         fr.onload = function () {
-            document.getElementById("test").src = fr.result;
+            const thisPage = pageManager.addCardHere(); //document.getElementById( page+pageID );
+            thisPage.appendChild( createHTML_Card( fr.result ) );
+            listenersUpdate();
         };
         fr.readAsDataURL( file );
     };
+};
 
-    // document.getElementById("test").src = file;
+function clearFile(e) {
+    console.log("this registering??");
+    e.target.value = "";
+};
 
+// PAGE MANAGER
+const pageManager = ( function () {
+    const maxPerPage = 9;
+    const page = "page";
+    let pageID = 0;
+
+    const addCardHere = function() {
+        let thisPage = document.getElementById( page+pageID );
+
+        console.log(`children elements ${thisPage.childElementCount} // max per page ${maxPerPage}`)
+
+        if (thisPage.childElementCount >= maxPerPage) {
+            pageID += 1;
+            
+            let pageParent = createHTML_Page( page+pageID );
+            document.querySelector("body").appendChild( pageParent );
+            thisPage = pageParent.querySelector(".paper");
+            console.log("MAKE A NEW PAGE PLEASE!");
+        }
+
+        return thisPage;
+    };
+
+    // SHIFTS CARDS SO THAT IT ALWAYS USES THE
+    // FEWEST NUMBER OF PAGES
+    const shiftCards = function () {
+        for (let i = pageID-1; i >= 0; i--) {
+
+            let pageA = document.getElementById( page+i );
+            let pageB = document.getElementById( page+(i+1) );
+
+            if (pageA){ console.log("found page A") }
+            if (pageB){ console.log("found page B") }
+
+
+
+            if (pageA && pageB) {
+
+                while( (pageA.childElementCount < maxPerPage) && (pageB.childElementCount > 0) ) {
+                    pageA.appendChild( pageB.childNodes[0] );
+                };
+
+                if (pageB.childElementCount <=0) {
+                    pageB.parentNode.remove();
+                    pageID -= 1;
+                };
+            };
+
+
+        }
+
+    };
+
+    return { addCardHere, shiftCards }
+
+})();
+
+function deleteCard(e) {
+    // TARGET THAT WILL BE REASSIGNED
+    let target = e.currentTarget;
+
+    // TRAVEL UP THE DOM UNTIL FIRST INSTANCE OF DIV WITH AN ID
+    while(target.id == '') {
+        target = target.parentNode;
+    };
+
+    target.remove();
+    pageManager.shiftCards();
+};
+
+
+
+
+function duplicateCard(e) {
+    // TARGET THAT WILL BE REASSIGNED
+    let target = e.currentTarget;
+
+    // TRAVEL UP THE DOM UNTIL FIRST INSTANCE OF DIV WITH AN ID
+    while(target.id == '') {
+        target = target.parentNode;
+    };
+
+    let dupe = target.cloneNode(true);
+    dupe.id = card + cardID;
+    cardID += 1;
+    
+    
+    pageManager.addCardHere().appendChild( dupe );
+    listenersUpdate();
+};
+
+function createHTML( type, classes) {
+    let element = document.createElement( type );
+    for(const el of classes) {
+        element.classList.add( el);
+    };
+    return element;
+};
+
+function createHTML_Page( pageID ) {
+
+    // <div class="page break">
+    //     <div class="paper" id = "page0">
+    //     </div>
+    // </div>
+
+    const pageDiv = createHTML("div", ["page", "break"]);
+    const paperDiv = createHTML("div", ["paper"]);
+    paperDiv.id = pageID;
+    pageDiv.appendChild( paperDiv );
+
+    return pageDiv;
+};
+
+function createHTML_Card( img ) {
+
+    // <div class="card">
+    //     <div class="buttons">
+    //         <button class="delete"> <img src="./images/delete.svg" alt=""> </button>
+    //         <button class="duplicate"> <img src="./images/duplicate.svg" alt=""> </button>
+    //     </div>
+    //     <img src="./images/007-001.webp" alt="" id="">
+    // </div>
+
+
+    const cardDiv = createHTML("div", ["card"]);
+    cardDiv.id = card+cardID;
+    cardID += 1;
+    const buttonsDiv = createHTML("div", ["buttons"]);
+    const deleteButton = createHTML("button", ["delete"]);
+    const deleteImg = createHTML("img", []);
+    deleteImg.src = "./images/delete.svg";
+    const duplicateButton = createHTML("button", ["duplicate"]);
+    const duplicateImg = createHTML("img", []);
+    duplicateImg.src = "./images/duplicate.svg";
+    const cardImg = createHTML("img", []);
+    cardImg.src = img;
+
+    deleteButton.appendChild(deleteImg);
+    duplicateButton.appendChild(duplicateImg);
+    buttonsDiv.appendChild(deleteButton);
+    buttonsDiv.appendChild(duplicateButton);
+
+    cardDiv.appendChild(buttonsDiv);
+    cardDiv.appendChild(cardImg);
+    return cardDiv;
 }
+
+
+function listenersUpdate() {
+    document.querySelectorAll(".delete").forEach( delButton => delButton.addEventListener("click", deleteCard ) );
+    document.querySelectorAll(".duplicate").forEach( delButton => delButton.addEventListener("click", duplicateCard ) );
+
+};
+
+function listenersCreate() {
+    document.getElementById("card-upload").addEventListener('click', clearFile);
+    document.getElementById("card-upload").addEventListener('change', upload);
+
+    listenersUpdate();
+};
+
+
+setup();
